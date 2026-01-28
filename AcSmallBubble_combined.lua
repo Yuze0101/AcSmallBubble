2c3edab304eb8a710c55c52f55ce0f83426dbf3c
gy <gengy5517@gmail.com>
2026-01-28 14:46:48 +0800

    update: 先清除画布

--- AcSmallBubble_combined.lua
+++ AcSmallBubble_combined.lua
@@ -1,5 +1,5 @@
 -- Auto-generated single file build
--- Generated at 2026-01-28 14:40:37
+-- Generated at 2026-01-28 14:46:45
 -- Original modules combined: utils, driverTable, render, main
 
 -- Define globals
@@ -91,7 +91,9 @@ local function renderCustom(carData)
     if driverTable[carData.index] then
         local driverData = driverTable[carData.index]
         local canvas, carInfo, distance = driverData.canvas, driverData.carInfo, driverData.distance
-
+        -- 先清除画布 防止文字重叠
+        canvas:clear()
+        -- 更新画布
         canvas:update(function()
             renderName(carInfo)
             renderDistance(distance)
